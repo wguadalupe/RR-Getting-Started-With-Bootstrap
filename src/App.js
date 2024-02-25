1146e0acac167bf0f07e1867c21fee00e5f41492
wguadalupe <w.guadalupe78@gmail.com>
2024-02-25 13:32:58 -0500

    built a form with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
 import './App.css';
-
-
+import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Bootstrap Website</h1>
-     
+    <div> {/* Enclose all content in a single parent div */}
+      <Nav activeKey="/home">
+        <Nav.Item>
+          <Nav.Link href="/home">Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">About Us</Nav.Link>
+        </Nav.Item>
+      </Nav>
+
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+
+        <Form.Group>
+          <Form.Label>Form Dropdown with favorite movies</Form.Label>
+          <Form.Control as="select">
+            <option>Inception</option>
+            <option>Forrest Gump</option>
+            <option>Shawshank Redemption</option>
+            <option>Spiderman: Into the Spiderverse</option>
+            <option>Avengers: Endgame</option>
+          </Form.Control>
+        </Form.Group>
+      </Form>
     </div>
   );
 }
